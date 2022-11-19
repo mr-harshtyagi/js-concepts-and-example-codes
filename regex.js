f2edbf6a9bffc0d787e58a59435c3f86f41c8a61
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-11-19 05:51:29 +0530

    first commit

---
+++ regex.js
@@ -0,0 +1,3 @@
+// How to use regex (regular expressions) in programming - super powerful but don't abuse,otherwise can lead to unmaintable code
+// can be used to add, remove, isolate, and manipulate all kinds of text and data
+// Industry use cases: string manipulation and testing in web scraping, validation frontend logic
